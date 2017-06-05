106b8a212d4e4b2db3a7dd0b4c9738cd4c4beed7
Fanfarlo <miguelo.mgmt@gmail.com>
2017-06-05 12:21:11 -0500

    Adding mobile touch event

--- js/main.js
+++ js/main.js
@@ -29,10 +29,17 @@ displayMenu();
 
 
 var mouseEvents = function(){
-  $("ul li").on("touchenter mouseover", function() {
-    $('ul div').toggleClass('nav-btns')
-}).on("touchleave mouseleave", function() {
-    // do hover end code
-});
+  $('a.taphover').on("touchstart", function (e) {
+      "use strict"; //satisfy the code inspectors
+      var link = $(this); //preselect the link
+      if (link.hasClass('hover')) {
+          return true;
+      } else {
+          link.addClass("hover");
+          $('a.taphover').not(this).removeClass("hover");
+          e.preventDefault();
+          return false; //extra, and to make sure the function has consistent return points
+      }
+  });
 }
 mouseEvents();
