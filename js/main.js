874c00e72c3e0e89d0acd679be3ed31caa44facb
Fanfarlo <miguelo.mgmt@gmail.com>
2017-06-22 19:09:36 -0500

    Added nav social

--- js/main.js
+++ js/main.js
@@ -49,3 +49,19 @@ var mouseEvents = function(){
   });
 }
 mouseEvents();
+
+
+
+  //Use smooth scrolling when clicking on navigation
+  $('.nav-desk a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') ===  this.pathname.replace(/^\//,'') && location.hostname ===  this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrolling
